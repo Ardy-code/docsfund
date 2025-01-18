04a14631ed17bbd20962c19f5bc40146a1077e3a
Ardy-code <ardy9948@gmail.com>
2025-01-18 19:01:18 +0700

    Susu

--- settings/documents.ts
+++ settings/documents.ts
@@ -223,47 +223,4 @@ export const Documents: Paths[] = [
   {
     spacer: true,
   },
-  {
-    title: "Markdown",
-    href: "/markdown",
-    heading: "Components",
-    items: [
-      {
-        title: "Cards",
-        href: "/cards",
-      },
-      {
-        title: "Diagrams",
-        href: "/diagrams",
-      },
-      {
-        title: "Filetree",
-        href: "/filetree",
-      },
-      {
-        title: "Lists",
-        href: "/lists",
-      },
-      {
-        title: "Maths",
-        href: "/maths",
-      },
-      {
-        title: "Notes",
-        href: "/notes",
-      },
-      {
-        title: "Steps",
-        href: "/steps",
-      },
-      {
-        title: "Table",
-        href: "/table",
-      },
-      {
-        title: "Tabs",
-        href: "/tabs",
-      },
-    ],
-  },
 ]
